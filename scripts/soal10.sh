5c36a0396a27435119d70b910533a3d160b6bd80
t4rouuu <164975247+t4rouuu@users.noreply.github.com>
2026-09-18 15:32:42 +0700

    Delete directory

--- scripts/soal10.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-apk add busybox-extras
-id phantom_user >/dev/null 2>&1 || adduser -D phantom_user
-echo 'phantom_user:phantom_user' | chpasswd
-pkill telnetd 2>/dev/null || true
-telnetd -p 23 -l /bin/login
-echo 'Telnet aktif pada port 23.'
--- scripts/soal3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-# Ganti eth0 jika interface menuju Internet berbeda.
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -o eth0 -j ACCEPT
-iptables -t nat -L -n -v
